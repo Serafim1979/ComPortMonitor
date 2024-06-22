688d149dc4f09d33127a53109c4f8d17202f59d8
Ivan Korolkov <korolkov.ivan@mail.ru>
2024-06-22 16:14:38 +0300

    Start development appliction

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <iostream>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+
+    }
+}
